13fff56c5c5d9d7a74d8f645be34096c2973ca96
Edoka Isaac <edokaizkdaking247@gmail.com>
2025-06-12 21:43:40 +0100

    Created MarketPlace contract

---
+++ src/Market.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.29;
+
+
+// Get rid of strings inside custom errors
+contract Store {
+    uint256 ItemCount;
+
+    error ValidPrice(string reason);
+    error InvalidQuantity(string reason);
+    error NotAvailable(string reason);
+    error OutOfStock(string reason);
+    error WrongAmount(string reason);
+    error NotOwner(string reason);
+
+    struct Item {
+        address payable vendor;
+        string name;
+        uint256 price;
+        uint128 quantity;
+    }
+
+    mapping(uint256 => Item) public items;
+
+    event ItemAdded(uint256 indexed ID, address indexed vendor, string name, uint256 price, uint128 quantity);
+
+    function AddItem(string memory _name, uint256 _price, uint128 _quantity) external {
+        if (_price <= 0) revert ValidPrice("Price Can Not Be Zero");
+        if (_quantity <= 0) revert InvalidQuantity("Quantity Can Not Be Zero");
+
+        items[ItemCount] = Item({
+            vendor: payable(msg.sender),
+            name: _name,
+            price: _price,
+            quantity: _quantity
+        });
+
+        emit ItemAdded(ItemCount, msg.sender, _name, _price, _quantity);
+
+        ItemCount++;
+    }
+
+    function PurchaseItem(uint256 ID, uint128 _quantity) external payable {
+        if (ID >= ItemCount) revert NotAvailable("Item Not In Stock");
+        if (_quantity <= 0) revert InvalidQuantity("Quantity Can Not Be Zero");
+
+        Item storage item = items[ID];
+
+        if (item.quantity < _quantity) revert OutOfStock("Not Enough In Stock");
+
+        uint256 totalPrice = item.price * _quantity;
+        if (msg.value != totalPrice) revert WrongAmount("Incorrect Amount To Pay");
+
+        item.quantity -= _quantity;
+        item.vendor.transfer(totalPrice);
+    }
+
+    function UpdateItemPrice(uint256 ID, uint256 _newPrice) external {
+        if (ID >= ItemCount) revert NotAvailable("Item Not In Stock");
+        if (_newPrice <= 0) revert ValidPrice("Price Can Not Be Zero");
+
+        Item storage item = items[ID];
+
+        if (item.vendor != msg.sender) revert NotOwner("You Are Not The Owner Of The Product");
+        item.price = _newPrice;
+    }
+
+    function RestockItems(uint256 ID, uint128 _quantity) external {
+        if (ID >= ItemCount) revert NotAvailable("Item Not In Stock");
+        if (_quantity <= 0) revert InvalidQuantity("Quantity Can Not Be Zero");
+        
+        Item storage item = items[ID];
+        if (item.vendor != msg.sender) revert NotOwner("You Are Not The Owner Of The Product");
+        
+        item.quantity += _quantity;
+    }
+
+    function ViewItem(uint256 ID) external view returns (Item memory) {
+        if (ID >= ItemCount) revert NotAvailable("Item Not In Stock");
+
+        return items[ID];
+    }
+}
---
+++ test/Market.t.sol
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.29;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Store} from "../src/Market.sol";
+
+contract TestMarketContract is Test {
+    Store store;
+    address vendor;
+    address buyer;
+    uint8 constant DECIMALS = 18;
+
+    function setUp() public {
+        store = new Store();
+        vendor = address(0x123);
+        buyer = address(0x124);
+
+        vm.deal(vendor, 5 * (10 ** DECIMALS));
+        vm.deal(buyer, 5 * (10 ** DECIMALS));
+    }
+
+    function testAddItem() public {
+        vm.prank(vendor);
+        store.AddItem("Shirt", 1 * (10 ** DECIMALS), 10);
+        (address Vendor, string memory name, uint256 price, uint128 quantity) = store.items(0);
+
+        assertEq(Vendor, vendor);
+        assertEq(name, "Shirt");
+        assertEq(price, 1 * (10 ** DECIMALS));
+        assertEq(quantity, 10);
+    }
+
+    function testAddItemWithZeroPrice() public {
+        vm.prank(vendor);
+        vm.expectRevert(abi.encodeWithSelector(Store.ValidPrice.selector, "Price Can Not Be Zero"));
+        store.AddItem("Cocaine", 0, 5);
+    }
+
+    function testPurchaseItem() public {
+        vm.prank(vendor);
+        store.AddItem("Shirt", 1 * (10 ** DECIMALS), 5);
+
+        vm.prank(buyer);
+        store.PurchaseItem{value: 2 * (10 ** DECIMALS)}(0, 2);
+
+        (, , , uint128 quantityLeft) = store.items(0);
+        assertEq(quantityLeft, 3);
+    }
+
+    function testPurchaseAllStock() public {
+        vm.prank(vendor);
+        store.AddItem("Shoes", 1 * (10 ** DECIMALS), 2);
+
+        vm.prank(buyer);
+        store.PurchaseItem{value: 2 * (10 ** DECIMALS)}(0, 2);
+
+        (, , , uint128 quantityLeft) = store.items(0);
+        assertEq(quantityLeft, 0);
+    }
+
+    function testPurchaseItemWithZeroQuantity() public {
+        vm.prank(vendor);
+        store.AddItem("Shirt", 1 * (10 ** DECIMALS), 5);
+
+        vm.prank(buyer);
+        vm.expectRevert(abi.encodeWithSelector(Store.InvalidQuantity.selector, "Quantity Can Not Be Zero"));
+        store.PurchaseItem{value: 1 * (10 ** DECIMALS)}(0, 0);
+    }
+
+    function testPurchaseMoreThanInStock() public {
+        vm.prank(vendor);
+        store.AddItem("Cap", 1 * (10 ** DECIMALS), 2);
+
+        vm.prank(buyer);
+        vm.expectRevert(abi.encodeWithSelector(Store.OutOfStock.selector, "Not Enough In Stock"));
+        store.PurchaseItem{value: 3 * (10 ** DECIMALS)}(0, 3);
+    }
+
+    function testUpdateItemPrice() public {
+        vm.prank(vendor);
+        store.AddItem("Shoes", 2 * (10 ** DECIMALS), 5);
+
+        vm.prank(vendor);
+        store.UpdateItemPrice(0, 3 * (10 ** DECIMALS));
+
+        (, , uint256 price, ) = store.items(0);
+        assertEq(price, 3 ether);
+    }
+
+    function testUpdateItemPriceNotOwner() public {
+        vm.prank(vendor);
+        store.AddItem("Bag", 1 * (10 ** DECIMALS), 5);
+
+        vm.prank(buyer);
+        vm.expectRevert(abi.encodeWithSelector(Store.NotOwner.selector, "You Are Not The Owner Of The Product"));
+        store.UpdateItemPrice(0, 2 * (10 ** DECIMALS));
+    }
+
+    function testRestockItem() public {
+        vm.prank(vendor);
+        store.AddItem("Pen", 1 * (10 ** DECIMALS), 5);
+
+        vm.prank(vendor);
+        store.RestockItems(0, 10);
+
+        (, , , uint128 quantity) = store.items(0);
+        assertEq(quantity, 15);
+    }
+
+    function testRestockNotOwner() public {
+        vm.prank(vendor);
+        store.AddItem("Charger", 1 * (10 ** DECIMALS), 3);
+
+        vm.prank(buyer);
+        vm.expectRevert(abi.encodeWithSelector(Store.NotOwner.selector, "You Are Not The Owner Of The Product"));
+        store.RestockItems(0, 2);
+    }
+}
